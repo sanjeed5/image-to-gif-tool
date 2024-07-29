a53e32cc83334cd3502b8abc2298f1dc813e9d45
sanjeed5 <msanjeed5@gmail.com>
2024-07-30 02:12:13 +0530

    workerscript loading fix

--- src/ImageToGifTool.js
+++ src/ImageToGifTool.js
@@ -60,7 +60,7 @@ const ImageToGifTool = () => {
       quality: 10,
       width: 300,
       height: 200,
-      workerScript: 'flipgifs.sanjeed.in/gif.worker.js',
+      workerScript: '/gif.worker.js',
     });
 
     const canvas = canvasRef.current;
